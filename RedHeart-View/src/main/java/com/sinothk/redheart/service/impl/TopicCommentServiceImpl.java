f76a7939cc0004dcf1b2c45a94603d5b2b14aa76
LiangYT <381518188@qq.com>
2019-11-01 17:03:19 +0800

    完成最新话题

--- RedHeart-View/src/main/java/com/sinothk/redheart/service/impl/TopicCommentServiceImpl.java
+++ RedHeart-View/src/main/java/com/sinothk/redheart/service/impl/TopicCommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sinothk.redheart.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sinothk.base.entity.PageData;
@@ -7,11 +8,14 @@ import com.sinothk.base.entity.ResultData;
 import com.sinothk.redheart.domain.TopicAo;
 import com.sinothk.redheart.domain.TopicCommentEntity;
 import com.sinothk.redheart.domain.TopicCommentVo;
+import com.sinothk.redheart.domain.TopicEntity;
 import com.sinothk.redheart.repository.TopicCommentMapper;
+import com.sinothk.redheart.repository.TopicMapper;
 import com.sinothk.redheart.service.TopicCommentService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 @Service("topicCommentService")
@@ -20,10 +24,23 @@ public class TopicCommentServiceImpl implements TopicCommentService {
     @Resource(name = "topicCommentMapper")
     private TopicCommentMapper topicCommentMapper;
 
+    @Resource(name = "topicMapper")
+    private TopicMapper topicMapper;
+
     @Override
     public ResultData<Boolean> add(TopicCommentEntity topicCommentEntity) {
         try {
             topicCommentMapper.insert(topicCommentEntity);
+
+            UpdateWrapper<TopicEntity> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.lambda().eq(TopicEntity::getTopicId, topicCommentEntity.getTopicId());
+
+            TopicEntity topicEntity = new TopicEntity();
+            topicEntity.setTopicId(topicCommentEntity.getTopicId());
+            topicEntity.setUpdateTime(new Date());
+
+            topicMapper.update(topicEntity, updateWrapper);
+
             return ResultData.success(true);
         } catch (Exception e) {
             return ResultData.error(e.getCause().getMessage());
