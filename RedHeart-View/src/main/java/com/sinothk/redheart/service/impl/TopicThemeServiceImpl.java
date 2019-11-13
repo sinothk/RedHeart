655fc6d0dd9885a5f0f68ba2a23981442eddbce3
LiangYT <381518188@qq.com>
2019-11-13 18:49:49 +0800

    完善功能！

--- RedHeart-View/src/main/java/com/sinothk/redheart/service/impl/TopicThemeServiceImpl.java
+++ RedHeart-View/src/main/java/com/sinothk/redheart/service/impl/TopicThemeServiceImpl.java
@@ -79,7 +79,7 @@ public class TopicThemeServiceImpl implements TopicThemeService {
             topicTheme.setAccount(account);
             topicTheme.setThemeCode("");
             topicTheme.setThemeTxt("全部");
-            topicTheme.setThemeIcon("living/10001/sysem/201911/theme_all2.jpg");
+            topicTheme.setThemeIcon("living/9999/sysem/201911/theme_all2.jpg");
             topicTheme.setRemark("1.5");
             list.add(0, topicTheme);
 
