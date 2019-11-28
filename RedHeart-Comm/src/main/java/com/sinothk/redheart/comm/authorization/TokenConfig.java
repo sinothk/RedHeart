801af076bfeb618a3f110edceed4118b10cf9684
LiangYT <381518188@qq.com>
2019-11-28 18:21:43 +0800

    新增极光推送：完善逻辑

--- RedHeart-Comm/src/main/java/com/sinothk/redheart/comm/authorization/TokenConfig.java
+++ RedHeart-Comm/src/main/java/com/sinothk/redheart/comm/authorization/TokenConfig.java
@@ -1,6 +1,8 @@
 package com.sinothk.redheart.comm.authorization;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,4 +18,17 @@ public class TokenConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authorizationInterceptor).addPathPatterns("/**");
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        // https://blog.csdn.net/com_it/article/details/95083104
+        // 为了解决 VueJS 跨域问题
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**"); //对应的接口
+            }
+        };
+    }
+
 }
