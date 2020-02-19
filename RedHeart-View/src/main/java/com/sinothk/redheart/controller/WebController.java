8c3d7fc772f73145806b8518eb6e48d7597a940d
LiangYT <381518188@qq.com>
2020-02-19 21:41:59 +0800

    新增访客数据

--- RedHeart-View/src/main/java/com/sinothk/redheart/controller/WebController.java
+++ RedHeart-View/src/main/java/com/sinothk/redheart/controller/WebController.java
@@ -3,12 +3,25 @@ package com.sinothk.redheart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
 @Controller
 public class WebController {
 
-    @GetMapping("/hello")
-    private String helloWorld() {
-        // http://localhost:80/redHeart/hello
-        return "hello";
+    @GetMapping("/index")
+    private String index() {
+        // http://localhost:80/redHeart/index
+        return "index";
+    }
+
+    @GetMapping("/main")
+    private String main(Map<String, Object> paramMap) {
+        // http://localhost:80/redHeart/main
+
+        /** 默认Map的内容会放大请求域中，页面可以直接使用Thymeleaf取值*/
+        paramMap.put("name", "张三");
+        paramMap.put("age", 35);
+
+        return "main";
     }
 }
