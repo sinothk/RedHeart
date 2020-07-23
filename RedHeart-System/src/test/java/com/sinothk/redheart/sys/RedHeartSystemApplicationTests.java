3fcecece26dd9e37313e316b3c2d0bd978fdd048
LiangYT <381518188@qq.com>
2020-07-23 20:11:55 +0800

    新增访客数据

--- RedHeart-System/src/test/java/com/sinothk/redheart/sys/RedHeartSystemApplicationTests.java
+++ RedHeart-System/src/test/java/com/sinothk/redheart/sys/RedHeartSystemApplicationTests.java
@@ -1,9 +1,7 @@
 package com.sinothk.redheart.sys;
 
-import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
