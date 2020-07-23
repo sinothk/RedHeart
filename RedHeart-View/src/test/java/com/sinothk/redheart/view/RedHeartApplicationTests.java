005f33643ca44c7a95b597ddd0fb6976e8ae43fa
LiangYT <381518188@qq.com>
2020-07-23 20:10:29 +0800

    新增访客数据

--- RedHeart-View/src/test/java/com/sinothk/redheart/view/RedHeartApplicationTests.java
+++ RedHeart-View/src/test/java/com/sinothk/redheart/view/RedHeartApplicationTests.java
@@ -1,13 +1,10 @@
 package com.sinothk.redheart.view;
 
-import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
 public class RedHeartApplicationTests {
 
     @Test
