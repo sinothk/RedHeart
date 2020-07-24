40ba3cb0063443acc5dadc64bb3d68cb8fc2975a
LiangYT <381518188@qq.com>
2020-07-24 14:06:40 +0800

    部署Jenkins调整！

--- RedHeart-View/src/main/java/com/sinothk/redheart/domain/KeyValue.java
+++ RedHeart-View/src/main/java/com/sinothk/redheart/domain/KeyValue.java
@@ -4,20 +4,20 @@ import java.util.HashMap;
 
 public class KeyValue {
     // 话题BIZ_TYPE
-    public static final String BIZ_TYPE_TOPIC = "BIZ_TYPE_TOPIC";
+    public final String BIZ_TYPE_TOPIC = "BIZ_TYPE_TOPIC";
 
     // APK
-    public static final String BIZ_TYPE_APK = "BIZ_TYPE_APK";
+    public final String BIZ_TYPE_APK = "BIZ_TYPE_APK";
 
     // 视频
-    public static final String BIZ_TYPE_VIDEO = "BIZ_TYPE_VIDEO";
+    public final String BIZ_TYPE_VIDEO = "BIZ_TYPE_VIDEO";
 
     /**
      * 话题主题分类
      */
-    private static HashMap<String, String> topicThemMap = null;
-    private static final String TOPIC_THEME_SEX = "TOPIC_THEME_SEX";
-    private static final String TOPIC_THEME_PLANT = "TOPIC_THEME_PLANT";
+    private HashMap<String, String> topicThemMap = null;
+    private final String TOPIC_THEME_SEX = "TOPIC_THEME_SEX";
+    private final String TOPIC_THEME_PLANT = "TOPIC_THEME_PLANT";
 
 //    public static String getTopicValue(String key) {
 //        if (topicThemMap == null) {
